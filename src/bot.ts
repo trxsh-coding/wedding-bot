83327838f2f80664cdd7aa738f37cfc85ccfba48
Orest Knxwn <orest@200apps.com>
2025-04-04 00:36:44 +0300

    ыыы

--- src/bot.ts
+++ src/bot.ts
@@ -10,8 +10,7 @@ const BOT_TOKEN = process.env.BOT_TOKEN!;
 const bot = new Telegraf(BOT_TOKEN);
 
 bot.start((ctx) => {
-    ctx.replyWithPhoto(
-        { source: imagePath },
+    ctx.replyWithPhoto('https://files.oaiusercontent.com/file-DD7e2QQDRb9MuhHmQSrJza?se=2025-04-03T21%3A33%3A01Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D299%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3Dimage.png&sig=lqHk//C6Mr84YHguKiAqYva7lQWqvscdN43pUe562y8%3D',
         {
             caption: 'Нажми на кнопку ниже 💖',
             reply_markup: {
